7aef92944e58cdda83e76c20cd9a5c69f9f3072b
gurbano <g.urbano@gmail.com>
2022-11-26 22:28:38 +0000

    export mfe component

--- next.config.js
+++ next.config.js
@@ -1,6 +1,33 @@
 /** @type {import('next').NextConfig} */
+const NextFederationPlugin = require('@module-federation/nextjs-mf');
+const path = require('path');
+
 const nextConfig = {
+  webpack(config, options) {
+    const { webpack } = options;
+    Object.assign(config.experiments, { topLevelAwait: true });
+    if (!options.isServer) {
+      //config.cache=false
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: 'mountains',
+          filename: 'static/chunks/remoteEntry.js',
+          exposes: {
+            // './react': 'react',
+            // './react-dom': 'react-dom',
+            './Mountains': './components/Mountains',
+          },
+          // shared: { react: { singleton: true }, 'react-dom': { singleton: true } },
+        }),
+      );
+    }
+
+    return config;
+  },
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 };
 
 module.exports = nextConfig;
